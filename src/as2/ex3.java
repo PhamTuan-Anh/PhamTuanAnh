16586f460a54089d18c236713ebcbd4b14079115
PhamTuan-Anh <panh75897@gmail.com>
2025-08-14 15:31:20 +0700

    anh

---
+++ src/as2/ex3.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package as2;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ex3 {
+     public static void main(String[] args){
+         int x = 1;
+         int y = x - x-- - x++ + --x - x + x-- + x;
+         System.out.println("x= "+x);
+         System.out.println("y= "+y);
+     }
+}
